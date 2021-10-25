0543f95da3c1697f2643b52c5e05dbd62a0f11bd
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-10-25 15:25:20 -0600

    added fetch to create new post

---
+++ public/js/newPost.js
@@ -0,0 +1,70 @@
+const saveButtonHandler = async (event) => {
+  event.preventDefault();
+
+  const id = event.target.getAttribute('data-postid');
+
+  // Collect values from the login form
+  const title = document.querySelector('#title').value.trim();
+  const content = document.querySelector('#content').value.trim();
+
+  if (title && content ) {
+    // Send a POST request to the API endpoint
+    const response = await fetch(`/api/posts/`, {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      
+      const result = await Swal.fire({
+        title: 'Congratulations!',
+        text: 'You successfully updated your Post!',
+        icon: 'success',
+        showCancelButton: false,
+        confirmButtonText: 'Thank you!',
+      })
+      
+      if (result.isConfirmed) {
+        document.location.replace(`/dashboard`);
+      } 
+
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const cancelButtonHandler = async (event) => {
+  event.preventDefault();
+
+  const result = await Swal.fire({
+    title: 'Are you sure?',
+    text: 'This will not save your post, and it will not be recoverable',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, cancel!',
+    cancelButtonText: 'No, I will complete'
+  })
+  
+  if (result.isConfirmed) {
+
+    document.location.replace(`/dashboard`);
+
+  } else if (result.dismiss === Swal.DismissReason.cancel) {
+    return
+  }
+
+};
+
+
+document
+  .querySelector('#create-post')
+  .addEventListener('click', saveButtonHandler);
+
+document
+  .querySelector('#cancel-post')
+  .addEventListener('click', cancelButtonHandler);
+
+
