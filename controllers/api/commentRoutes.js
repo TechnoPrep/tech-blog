a499ce6d0a300125e404dbdea65ac8fe5eebf529
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-10-25 15:21:02 -0600

    created route to manage creating, updating, and deleting comments

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      comment: req.body.comment,
+      post_id: req.body.postId,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.update({ 
+      comment: req.body.comment,
+    },
+    {
+      where: {
+        id: req.params.id,
+        post_id: req.body.postId,
+        user_id: req.session.user_id
+      }
+    })
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(commentData);
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
